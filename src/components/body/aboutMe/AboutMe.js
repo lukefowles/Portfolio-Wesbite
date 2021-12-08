e17a86aa76fe431054da34f0ff01c61700296ecd
Luke Fowles <lukefowles@Lukes-MacBook-Air.local>
2021-12-08 14:49:22 +0000

    forgot to save changes to aboutMe.js

--- src/components/body/aboutMe/AboutMe.js
+++ src/components/body/aboutMe/AboutMe.js
@@ -8,9 +8,7 @@ function AboutMe() {
             <br/>LUKE FOWLES<br/>
             </div>
             <div className = 'about-info'>
-            Hello, my name is Luke Fowles and this is my bio and I am going to write a stream of conciousness
-            to see how this looks otherwise it may look realy bad but we shall see I guess but I want to be a
-            software engineer.
+            Hello, my name is Luke Fowles and this is an example of where my bio would go in my portfolio website.
             </div>
         </div>
     )
